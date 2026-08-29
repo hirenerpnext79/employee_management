868c670d2aabdebef414d5333d62af13e5c64622
Hiren Savaliya <hirenerpnext79@gmail.com>
2026-08-29 13:58:08 +0530

    feat: implement VCard functionality with dynamic QR generation and introduce new HNS doctypes for web pages, attachments, and settings.

---
+++ employee_management/employee_management/doctype/vcard_setting/test_vcard_setting.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, HNS and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestVcardSetting(FrappeTestCase):
+	pass
---
+++ employee_management/employee_management/doctype/vcard_setting/vcard_setting.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, HNS and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Vcard Setting", {
+// 	refresh(frm) {
+
+// 	},
+// });
